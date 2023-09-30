e17cefb3a2f0067b82507bf721647417f4892f0e
Sandeep Guttula <81863963+sandeep-guttula@users.noreply.github.com>
2023-10-01 01:37:22 +0530

    Time: 0 ms (100%), Space: 57.5 MB (6.32%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+        int result = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > arr[mid + 1] && arr[mid - 1] < arr[mid]) {
+                result = mid;
+                break;
+            } else if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            } else if (arr[mid] > arr[mid + 1]) {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+}
